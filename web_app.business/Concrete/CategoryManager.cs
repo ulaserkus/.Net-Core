4432a9afd6dc604ce948b25a8d73e45ac3096d3e
Ulaş Hasan <67653664+ulaserkus@users.noreply.github.com>
2021-01-22 21:19:44 +0200

    Add files via upload

---
+++ web_app.business/Concrete/CategoryManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using web_app.business.Abstract;
+using web_app.data.Abstract;
+using web_app.entity;
+
+namespace web_app.business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        private ICategoryRepository CategoryRepository;
+        public CategoryManager(ICategoryRepository categoryRepository)
+        {
+            CategoryRepository = categoryRepository;
+        }
+        public void Create(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            return CategoryRepository.GetAll();
+        }
+
+        public Category GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ web_app.business/Concrete/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using web_app.business.Abstract;
+using web_app.data.Abstract;
+using web_app.entity;
+
+namespace web_app.business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductRepository _productRepository;
+        public ProductManager(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+
+        }
+
+        public void Create(Product entity)
+        {
+
+            //iş kurallarını  uygula
+            _productRepository.Create(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+            _productRepository.Delete(entity);
+        }
+
+        public List<Product> GetAll()
+        {
+          return  _productRepository.GetAll();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _productRepository.GetProductById(id);
+        }
+
+        public List<Product> GetProductsByCategory(string category)
+        {
+            return _productRepository.GetProductByCategory(category);
+        }
+
+        public void Update(Product entity)
+        {
+            _productRepository.Update(entity);
+        }
+
+     
+    }
+}
